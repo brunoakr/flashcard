cccf3de5875d0807a68019fa007d40df2bc55273
brunoakr <161325932+brunoakr@users.noreply.github.com>
2024-10-10 07:53:56 -0300

    eu gosto de homem

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao{
+'Historia',
+'EM que ano foi declarado a independência do Brasil',
+'1822'
+}
+
+criaCartao{
+    'Matemática',
+    'Qual é o resultado da raiz de 121',
+    '11'
+    }
+
+    criaCartao{
+        'Biologia',
+        'Quantos cromossomos tem um portador de Síndrome de Down',
+        '47 cromossomos'
+        }
+
+        criaCartao{
+            'Geografia',
+            'Quais são os três tipos de aquífero',
+            'Aquífero Livre, Aquífero Confinado e Aquífero Suspenso'
+            }
+
+            criaCartao{
+                'Educação Financeira',
+                'O que é taxa selic',
+                'É a taxa de juros média praticada nas operações compromissadas com títulos públicos federais com prazo de um dia útil'
+                }
+
+                criaCartao{
+                    'Químico',
+                    'Qual o número atômico da molécula de cloro',
+                    'É 17'
+                    }
